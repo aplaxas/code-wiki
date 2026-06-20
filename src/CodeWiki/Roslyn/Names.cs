933432caca7203b1f6f76959f2fc48aabd377c5a
aplaxas <aplaxas@gmail.com>
2026-06-19 22:44:43 -0500

    feat(codewiki): InterfaceImplementationExtractor(IMPLEMENTS_METHOD 허브)

--- src/CodeWiki/Roslyn/Names.cs
+++ src/CodeWiki/Roslyn/Names.cs
@@ -7,7 +7,8 @@ public static class Names
     private static readonly SymbolDisplayFormat Fmt = new(
         globalNamespaceStyle: SymbolDisplayGlobalNamespaceStyle.Omitted,
         typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces,
-        genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters);
+        genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters,
+        memberOptions: SymbolDisplayMemberOptions.IncludeContainingType);
 
     public static string Full(ISymbol s) => s.ToDisplayString(Fmt);
 }
